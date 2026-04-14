7bd235435ead2aa985af06efe4ed74aa9857958e
Ana Dieguez <adiejul560@g.educaand.es>
2026-04-14 10:09:57 +0200

    ajusto componentes.js en el cual, creo aqui la barra de navegacion y el footer, para llamarlo desde cada pagina, asi si modifico algo desde components, sale directamente en todas las paginas y no hay que cambiar una por una

--- frontend/js/componentes.js
+++ frontend/js/componentes.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <li><a href="reservas.html">Mis Reservas</a></li>
             <li><a href="nuevaReserva.html">Nueva Reserva</a></li>
             <li class="usuario-logeado">
-                <a href="perfil.html">
+                <a href="aCliente.html">
                     <span class="icono-user">👤</span> Hola, Juan Pérez
                 </a>
             </li>
